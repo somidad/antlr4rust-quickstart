0bc40c180d782d044097f68b756fdba6c5f82da0
Seokseong Jeon <sjeon87@gmail.com>
2020-06-02 12:43:47 +0900

    Store a parsed CSV structure into a variable

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate lazy_static;
 use antlr_rust::common_token_stream::CommonTokenStream;
 use antlr_rust::input_stream::InputStream;
 use antlr_rust::parser_rule_context::ParserRuleContext;
-use antlr_rust::tree::{ErrorNode, ParseTreeListener, TerminalNode};
+use antlr_rust::tree::{ErrorNode, ParseTree, ParseTreeListener, TerminalNode};
 
 mod csvlexer;
 mod csvlistener;
@@ -14,7 +14,19 @@ use csvlexer::CSVLexer;
 use csvlistener::CSVListener;
 use csvparser::*;
 
-struct Listener;
+type Row = Vec<String>;
+
+#[derive(Debug)]
+struct CSV {
+    header: Row,
+    rows: Vec<Row>,
+}
+
+struct Listener {
+    csv: Box<CSV>,
+    add_to_header: bool,
+    row_to_add: Vec<String>,
+}
 
 impl ParseTreeListener for Listener {
     fn visit_terminal(&mut self, _node: &TerminalNode) {}
@@ -26,12 +38,26 @@ impl ParseTreeListener for Listener {
 impl CSVListener for Listener {
     fn enter_csvFile(&mut self, _ctx: &CsvFileContext) {}
     fn exit_csvFile(&mut self, _ctx: &CsvFileContext) {}
-    fn enter_hdr(&mut self, _ctx: &HdrContext) {}
-    fn exit_hdr(&mut self, _ctx: &HdrContext) {}
+    fn enter_hdr(&mut self, _ctx: &HdrContext) {
+        self.add_to_header = true;
+    }
+    fn exit_hdr(&mut self, _ctx: &HdrContext) {
+        self.csv.header = self.row_to_add.to_vec();
+        self.row_to_add.clear();
+        self.add_to_header = false;
+    }
     fn enter_row(&mut self, _ctx: &RowContext) {}
-    fn exit_row(&mut self, _ctx: &RowContext) {}
+    fn exit_row(&mut self, _ctx: &RowContext) {
+        if self.add_to_header {
+            return;
+        }
+        self.csv.rows.push(self.row_to_add.to_vec());
+        self.row_to_add.clear();
+    }
     fn enter_field(&mut self, _ctx: &FieldContext) {}
-    fn exit_field(&mut self, _ctx: &FieldContext) {}
+    fn exit_field(&mut self, _ctx: &FieldContext) {
+        self.row_to_add.push(_ctx.get_text());
+    }
 }
 
 fn main() {
@@ -43,7 +69,17 @@ This, is, a, row
     let lexer = CSVLexer::new(Box::new(InputStream::new(input)));
     let token_source = CommonTokenStream::new(lexer);
     let mut parser = CSVParser::new(Box::new(token_source));
-    parser.add_parse_listener(Box::new(Listener {}));
+    let listener_id = parser.add_parse_listener(Box::new(Listener {
+        csv: Box::new(CSV {
+            header: Row::new(),
+            rows: Vec::new(),
+        }),
+        add_to_header: false,
+        row_to_add: Row::new(),
+    }));
     let result = parser.csvFile();
     assert!(result.is_ok());
+    let listener = parser.remove_parse_listener(listener_id);
+    let csv = listener.csv;
+    println!("{:#?}", csv);
 }
